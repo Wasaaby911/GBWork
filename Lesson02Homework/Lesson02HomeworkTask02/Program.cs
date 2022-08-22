7c55d355a2d91ed5a3c0fac823e8b73794b1d990
wasaaby911 <Wasaaby911@gmail.com>
2022-08-23 00:05:50 +0500

    Lesson02Homework

---
+++ Lesson02Homework/Lesson02HomeworkTask02/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите число");
+int value = int.Parse(Console.ReadLine());
+value = Math.Abs(value);
+
+int size = value.ToString().Length;
+if (size > 2)
+{
+
+    int[] Array = new int[size];
+
+    while (value != 0)
+    {
+        Array[size - 1] = value % 10;
+        value = value / 10;
+        size--;
+    }
+    Console.WriteLine($"Третья цифра числа: {Array[2]}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ Lesson02Homework/Lesson02HomeworkTask03/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int a = int.Parse(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Понедельник - рабочий день");
+        break;
+    case 2:
+        Console.WriteLine("Вторник - рабочий день");
+        break;
+    case 3:
+        Console.WriteLine("Среда - рабочий день");
+        break;
+    case 4:
+        Console.WriteLine("Четверг - рабочий день");
+        break;
+    case 5:
+        Console.WriteLine("Пятница - рабочий день");
+        break;
+    case 6:
+        Console.WriteLine("Суббота - выходной");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье - выходной");
+        break;
+    default:
+        Console.WriteLine("Ввод не корректный");
+        break;
+}
