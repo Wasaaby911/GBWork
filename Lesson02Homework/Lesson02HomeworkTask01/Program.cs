bc0e37d8b105e3bf856ff57dd77e8a1b79b68e81
wasaaby911 <Wasaaby911@gmail.com>
2022-08-23 19:03:39 +0500

    Fix // if (number > 99 && number < 1000)

--- Lesson02Homework/Lesson02HomeworkTask01/Program.cs
+++ Lesson02Homework/Lesson02HomeworkTask01/Program.cs
@@ -3,7 +3,7 @@
 
 Console.WriteLine($"Введите трехзначное число");
 int number = int.Parse(Console.ReadLine());
-if (number > 100 && number < 1000)
+if (number > 99 && number < 1000)
 {
     number = (number / 10) % 10;
     Console.WriteLine($"вторая цифра заданного числа: {number}");
